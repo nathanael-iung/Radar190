53e601c1b1ebb9e57bd4c9ee3240fe967b313f0a
Jean Benkendorf Jean Benkendorf <jean.benkendorf@outlook.com>
2018-08-30 21:05:00 -0300

    Cadastro_Chamar demais paginas

--- Radar190/ViewsWPF/Views/Cadastro.xaml.cs
+++ Radar190/ViewsWPF/Views/Cadastro.xaml.cs
@@ -36,9 +36,77 @@ namespace ViewsWPF.Views
             Cadastro.Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+  
+        private void btnCadastroHome_Click(object sender, RoutedEventArgs e)
         {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
 
+            Index CadastroparaHome = new Index();
+            CadastroparaHome.Show();
+        }
+
+        private void btnCadastroDenuncie_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Denuncie CadastroparaDenuncie = new Denuncie();
+            CadastroparaDenuncie.Show();
+        }
+
+        private void btnCadastroMapa_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Mapa CadastroparaMapa = new Mapa();
+            CadastroparaMapa.Show();
+        }
+
+        private void btnCadastroEstatisticas_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Estatisticas CadastroparaEstatisticas = new Estatisticas();
+            CadastroparaEstatisticas.Show();
+        }
+
+        private void btnCadastroChat_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Chat CadastroparaChat = new Chat();
+            CadastroparaChat.Show();
+        }
+
+        private void btnCadastroContatosEmergencia_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Contatos_Emergencias CadastroparaContatosEmergenciais = new Contatos_Emergencias();
+            CadastroparaContatosEmergenciais.Show();
+        }
+
+        private void btnCadastroDicas_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Dicas_de_Seguranca CadastroparaDicasSeguranca = new Dicas_de_Seguranca();
+            CadastroparaDicasSeguranca.Show();
+        }
+
+        private void btnCadastroFaleConosco_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro ChatFechar = new Cadastro();
+            this.Close();
+
+            Fale_Conosco CadastroparaFaleConosco = new Fale_Conosco();
+            CadastroparaFaleConosco.Show();
         }
     }
 }
