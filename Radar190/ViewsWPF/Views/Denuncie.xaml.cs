6d4ea005746724b9e0ae3aedd426097c2d677185
Jean Benkendorf Jean Benkendorf <jean.benkendorf@outlook.com>
2018-08-30 21:51:40 -0300

    Denuncie_Chamar demais telas

--- Radar190/ViewsWPF/Views/Denuncie.xaml.cs
+++ Radar190/ViewsWPF/Views/Denuncie.xaml.cs
@@ -28,5 +28,86 @@ namespace ViewsWPF.Views
         {
 
         }
+
+        private void btnDenuncieHome_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Index DenuncieparaHome = new Index();
+            DenuncieparaHome.Show();
+        }
+
+        private void btnDenuncieCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Cadastro DenuncieparaCadastro = new Cadastro();
+            DenuncieparaCadastro.Show();
+        }
+
+        private void btnDenuncieDenuncie_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Denuncie DenuncieparaDenuncie = new Denuncie();
+            DenuncieparaDenuncie.Show();
+        }
+
+        private void btnDenuncieMapa_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Mapa DenuncieparaMapa = new Mapa();
+            DenuncieparaMapa.Show();
+        }
+
+        private void btnDenuncieEstatisticas_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Estatisticas DenuncieparaEstatistica = new Estatisticas();
+            DenuncieparaEstatistica.Show();
+        }
+
+        private void btnDenuncieChat_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Chat DenuncieparaChat = new Chat();
+            DenuncieparaChat.Show();
+        }
+
+        private void btnDenuncieContatosEmergencias_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Contatos_Emergencias DenuncieparaContatosEmergenciais = new Contatos_Emergencias();
+            DenuncieparaContatosEmergenciais.Show();
+        }
+
+        private void btnDenuncieDicas_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Dicas_de_Seguranca DenuncieparaDicas = new Dicas_de_Seguranca();
+            DenuncieparaDicas.Show();
+        }
+
+        private void btnDenuncieFaleConosco_Click(object sender, RoutedEventArgs e)
+        {
+            Denuncie ChatFechar = new Denuncie();
+            this.Close();
+
+            Fale_Conosco DenuncieparaFaleConosco = new Fale_Conosco();
+            DenuncieparaFaleConosco.Show();
+        }
     }
 }
