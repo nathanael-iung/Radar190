ea63a8d3d9cf487e03e4f13c812bde92b18f4ce0
Jean Benkendorf <jean.benkendorf@outlook.com>
2018-09-01 19:34:38 -0300

    Mapa_Chamar demais telas

--- Radar190/ViewsWPF/Views/Mapa.xaml.cs
+++ Radar190/ViewsWPF/Views/Mapa.xaml.cs
@@ -29,5 +29,77 @@ namespace ViewsWPF.Views
             //Abertura do MyMaps direto na tela com as marcações locais
             wbMaps.Source = new Uri("https://www.google.com/maps/d/embed?mid=1qqtQqSmTczMvEoBiuduaQE4uDY2U4RBi");
         }
+
+        private void btnMapaHome_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Index MapaparaHome = new Index();
+            MapaparaHome.Show();
+        }
+
+        private void btnMapaCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Cadastro MapaparaCadastro = new Cadastro();
+            MapaparaCadastro.Show();
+        }
+
+        private void btnMapaDenuncie_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Denuncie MapaparaDenuncie = new Denuncie();
+            MapaparaDenuncie.Show();
+        }
+
+        private void btnMapaEstatisticas_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Estatisticas MapaparaEstatisticas = new Estatisticas();
+            MapaparaEstatisticas.Show();
+        }
+
+        private void btnMapaChat_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Chat MapaparaChat = new Chat();
+            MapaparaChat.Show();
+        }
+
+        private void btnMapaContatosEmergenciais_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Contatos_Emergencias MapaparaContatosEmergenciais = new Contatos_Emergencias();
+            MapaparaContatosEmergenciais.Show();
+        }
+
+        private void btnMapaDicas_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Dicas_de_Seguranca MapaparaDicas = new Dicas_de_Seguranca();
+            MapaparaDicas.Show();
+        }
+
+        private void btnMapaFaleConosco_Click(object sender, RoutedEventArgs e)
+        {
+            Mapa MapaFechar = new Mapa();
+            this.Close();
+
+            Fale_Conosco MapaparaFaleConosco = new Fale_Conosco();
+            MapaparaFaleConosco.Show();
+        }
     }
 }
