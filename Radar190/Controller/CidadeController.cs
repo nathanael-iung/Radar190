815795c013e31e4a8ac5d6f8f6e3b9c7d64e348c
Nathanael Iung <nathanaeliung@hotmail.com>
2018-09-13 20:10:19 -0300

    Mudança no design do BD.

---
+++ Radar190/Controller/CidadeController.cs
@@ -0,0 +1,53 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class CidadeController
+    {
+        void Insert(Cidade cid)
+        {
+            BDRadarContainer contexto = new BDRadarContainer();
+            contexto.CidadeSet.Add(cid);
+            contexto.SaveChanges();
+        }
+
+        Cidade BuscaID(int id)
+        {
+            BDRadarContainer contexto = new BDRadarContainer();
+            return contexto.CidadeSet.Find();
+        }
+
+        void Edit(int id, Cidade cidEditado)
+        {
+            Cidade cidAntigo = BuscaID(id);
+
+            if (cidAntigo != null)
+            {
+                cidAntigo.City = cidEditado.City;
+                cidAntigo.UF = cidEditado.UF;
+
+                BDRadarContainer contexto = new BDRadarContainer();
+                contexto.Entry(cidAntigo).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+        void Delete(int id)
+        {
+            Cidade cidExcluir = new Cidade();
+
+            if (cidExcluir != null)
+            {
+                BDRadarContainer contexto = new BDRadarContainer();
+                contexto.CidadeSet.Remove(cidExcluir);
+                contexto.SaveChanges();
+            }
+        }
+
+    }
+}
